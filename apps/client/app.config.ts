fe9e591f6f96e5ec69a25443428bde4f76ce4237
vividsystem <notrealbutnotobot@gmail.com>
2025-09-11 12:46:11 +0200

    fix: module loading

--- apps/client/app.config.ts
+++ apps/client/app.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "@solidjs/start/config";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 export default defineConfig({
   vite: {
 		resolve: {
 			alias: {
-				"~": "./src"
+				"~": path.resolve(__dirname, "src")
 			}
 		},
     ssr: { external: ["drizzle-orm"] },
