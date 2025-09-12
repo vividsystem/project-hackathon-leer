28f8acaae6a1947d8a15af35a79cfb4c483ff5e8
vividsystem <notrealbutnotobot@gmail.com>
2025-09-12 18:34:13 +0200

    feat: added tailwindcss

--- apps/client/app.config.ts
+++ apps/client/app.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "@solidjs/start/config";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 export default defineConfig({
   vite: {
+		plugins: [tailwindcss()],
 		resolve: {
 			alias: {
 				"~": path.resolve(__dirname, "src")
